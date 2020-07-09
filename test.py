67da4edc27bb7399ca7ac2ba8ee31583348f2475
Igor Akulov <egor@akulovs.com>
2020-07-09 17:48:21 +0500

    added full test scenario

---
+++ test.py
@@ -0,0 +1,12 @@
+﻿from core.sberbankPDFtext2Excel import sberbankPDFtext2Excel
+import sys
+if len(sys.argv) < 2:
+    print('Недостаточно аргументов')
+    print(__doc__)
+    exit(1)
+elif len(sys.argv)==2:
+    outputFileName = None
+elif len(sys.argv)==3:
+    outputFileName=sys.argv[2]
+
+sberbankPDFtext2Excel(sys.argv[1], outputFileName)
