b38b958460bd9b1740836add97f324cf58b3b318
Ev2geny <ev2geny@gmail.com>
2021-11-13 21:21:14 +0100

    Some extra comments

--- core/no_github_module_import.py
+++ core/no_github_module_import.py
@@ -1,8 +1,16 @@
 """
 this module simply imports information, defined in the module no_github_module.py
 
+This module is only needed for testing
+
 no_github_module.py contains information, which is not shared via github due to confidential nature
 
+Its structure is following:
+
+SBER_DEBIT_old_not_supported_pdf = r"Path to some file on the drive"
+
+SBER_DEBIT_2005_pdf = r"Path to some other file on the drive"
+...
 
 """
 from no_github_module import SBER_DEBIT_old_not_supported_pdf
