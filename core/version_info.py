5fd24abd8dac22641b2c0336f34094d7ced44d38
Ev2geny <ev2geny@gmail.com>
2024-06-30 20:24:06 +0200

    Version updated. Prepared for merge with master

--- core/version_info.py
+++ core/version_info.py
@@ -1,4 +1,4 @@
 NAME='Конвертер выписки Сбербанка по карте из формата PDF в формат Excel'
 AUTHOR='ev2geny@gmail.com'
 PERMANENT_LOCATION='https://github.com/Ev2geny/Sberbank2Excel/releases/latest'
-VERSION='4.6.1'
+VERSION='4.7.0'
